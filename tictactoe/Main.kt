172bc09fd4f3794f4aa65ee9b4a817bbc88f7a4d
Denis <87063316+escapist-berlin@users.noreply.github.com>
2025-06-19 08:57:47 +0200

    feat: implement stage 2 of simple-tic-tac-toe project – render 3x3 grid from input string

--- tictactoe/Main.kt
+++ tictactoe/Main.kt
@@ -1,10 +1,16 @@
 package tictactoe
 
+import java.util.Scanner
+
 fun main() {
+    val scanner = Scanner(System.`in`)
+    val input = scanner.next()
     val output = """
-        X O X
-        O X O
-        X X O
+        ---------
+        | ${input[0]} ${input[1]} ${input[2]} |
+        | ${input[3]} ${input[4]} ${input[5]} |
+        | ${input[6]} ${input[7]} ${input[8]} |
+        ---------
     """.trimIndent()
 
     println(output)
